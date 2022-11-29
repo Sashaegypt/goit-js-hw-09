08f5305f8bab08f706cc62b4078989607cdd9aad
Sashaegypt <107362302+Sashaegypt@users.noreply.github.com>
2022-11-29 16:32:53 +0000

    Deploying to gh-pages from @ Sashaegypt/goit-js-hw-09@6bab7e0137e6ae59ce284882b0109adaa627b5d0 🚀

--- 01-color-switcher.05d425cf.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){n.disabled=!1,clearInterval(e)}))}();
-//# sourceMappingURL=01-color-switcher.05d425cf.js.map
--- 01-color-switcher.d41490a3.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){e.disabled=!1,clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.d41490a3.js.map
