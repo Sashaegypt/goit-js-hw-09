e1b06a3834c2bebeca76d012ace3355c5f25c720
Sashaegypt <107362302+Sashaegypt@users.noreply.github.com>
2022-11-29 17:19:33 +0100

    make first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    startBtn.disabled = true;
+    intervalId = setInterval(() => {
+       document.body.style.backgroundColor = getRandomHexColor(); 
+    }, 1000); 
+}
+
+function onStop(){
+ stopBtn.disabled = false;
+ clearInterval(intervalId);
+}
+// function onStart() {
+//     refs.startBtn.disabled = true;
+//     intervalId = setInterval(() => {
+//       document.body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+//   }
+//   function onStop() {
+//     refs.startBtn.disabled = false;
+//     clearInterval(intervalId);
+//   }
